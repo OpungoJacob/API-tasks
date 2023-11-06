d9adf8318373e6fa7968e68939f8253fd81bb3c3
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2023-11-06 18:01:34 +0300

    Update App.js

--- Spotifysearcher_API/src/App.js
+++ Spotifysearcher_API/src/App.js
@@ -4,7 +4,7 @@ import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-boo
 import { useState, useEffect } from 'react';
 
 const CLIENT_ID ="4656788a4330aa14b26aa5103670483668a";
-const CLIENT_SECRET ="ae443452d65b8fa4538bb6ab92531bf5ad8"
+const CLIENT_SECRET ="ae443452d65b8fa4538bb6ab92531bf5ad8";
 
 export default function App(){
 
